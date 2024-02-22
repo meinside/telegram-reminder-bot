10fada78f0a4e8cb2ab451416f9c090817fa2740
Sungjin Han <meinside@meinside.dev>
2024-02-22 14:43:49 +0900

    Fix to support config in JWCC JSON format

--- bot.go
+++ bot.go
@@ -18,6 +18,8 @@ import (
 	"github.com/meinside/openai-go"
 	tg "github.com/meinside/telegram-bot-go"
 	"github.com/meinside/version-go"
+
+	"github.com/tailscale/hujson"
 )
 
 const (
@@ -116,59 +118,72 @@ type arguments struct {
 func loadConfig(fpath string) (conf config, err error) {
 	var bytes []byte
 	if bytes, err = os.ReadFile(fpath); err == nil {
-		if err = json.Unmarshal(bytes, &conf); err == nil {
-			if (conf.TelegramBotToken == "" || conf.OpenAIAPIKey == "" || conf.OpenAIOrganizationID == "") && conf.Infisical != nil {
-				// read token and api key from infisical
-				var botToken, apiKey, orgID string
+		if bytes, err = standardizeJSON(bytes); err == nil {
+			if err = json.Unmarshal(bytes, &conf); err == nil {
+				if (conf.TelegramBotToken == "" || conf.OpenAIAPIKey == "" || conf.OpenAIOrganizationID == "") && conf.Infisical != nil {
+					// read token and api key from infisical
+					var botToken, apiKey, orgID string
 
-				var kvs map[string]string
-				kvs, err = helper.Values(
-					conf.Infisical.ClientID,
-					conf.Infisical.ClientSecret,
-					conf.Infisical.WorkspaceID,
-					conf.Infisical.Environment,
-					conf.Infisical.SecretType,
-					[]string{
-						conf.Infisical.TelegramBotTokenKeyPath,
-						conf.Infisical.OpenAIAPIKeyKeyPath,
-						conf.Infisical.OpenAIOrganizationIDKeyPath,
-					},
-				)
+					var kvs map[string]string
+					kvs, err = helper.Values(
+						conf.Infisical.ClientID,
+						conf.Infisical.ClientSecret,
+						conf.Infisical.WorkspaceID,
+						conf.Infisical.Environment,
+						conf.Infisical.SecretType,
+						[]string{
+							conf.Infisical.TelegramBotTokenKeyPath,
+							conf.Infisical.OpenAIAPIKeyKeyPath,
+							conf.Infisical.OpenAIOrganizationIDKeyPath,
+						},
+					)
 
-				var exists bool
-				if botToken, exists = kvs[conf.Infisical.TelegramBotTokenKeyPath]; exists {
-					conf.TelegramBotToken = botToken
+					var exists bool
+					if botToken, exists = kvs[conf.Infisical.TelegramBotTokenKeyPath]; exists {
+						conf.TelegramBotToken = botToken
+					}
+					if apiKey, exists = kvs[conf.Infisical.OpenAIAPIKeyKeyPath]; exists {
+						conf.OpenAIAPIKey = apiKey
+					}
+					if orgID, exists = kvs[conf.Infisical.OpenAIOrganizationIDKeyPath]; exists {
+						conf.OpenAIOrganizationID = orgID
+					}
 				}
-				if apiKey, exists = kvs[conf.Infisical.OpenAIAPIKeyKeyPath]; exists {
-					conf.OpenAIAPIKey = apiKey
-				}
-				if orgID, exists = kvs[conf.Infisical.OpenAIOrganizationIDKeyPath]; exists {
-					conf.OpenAIOrganizationID = orgID
-				}
-			}
 
-			// set default/fallback values
-			if conf.MonitorIntervalSeconds <= 0 {
-				conf.MonitorIntervalSeconds = defaultMonitorIntervalSeconds
-			}
-			if conf.TelegramIntervalSeconds <= 0 {
-				conf.TelegramIntervalSeconds = defaultTelegramIntervalSeconds
-			}
-			if conf.MaxNumTries <= 0 {
-				conf.MaxNumTries = defaultMaxNumTries
-			}
-			if conf.OpenAIModel == "" {
-				conf.OpenAIModel = defaultChatCompletionModel
-			}
-			if conf.DefaultHour < 0 {
-				conf.DefaultHour = 0
-			} else if conf.DefaultHour >= 24 {
-				conf.DefaultHour = 23
+				// set default/fallback values
+				if conf.MonitorIntervalSeconds <= 0 {
+					conf.MonitorIntervalSeconds = defaultMonitorIntervalSeconds
+				}
+				if conf.TelegramIntervalSeconds <= 0 {
+					conf.TelegramIntervalSeconds = defaultTelegramIntervalSeconds
+				}
+				if conf.MaxNumTries <= 0 {
+					conf.MaxNumTries = defaultMaxNumTries
+				}
+				if conf.OpenAIModel == "" {
+					conf.OpenAIModel = defaultChatCompletionModel
+				}
+				if conf.DefaultHour < 0 {
+					conf.DefaultHour = 0
+				} else if conf.DefaultHour >= 24 {
+					conf.DefaultHour = 23
+				}
 			}
 		}
 	}
 
 	return conf, err
+}
+
+// standardize given JSON (JWCC) bytes
+func standardizeJSON(b []byte) ([]byte, error) {
+	ast, err := hujson.Parse(b)
+	if err != nil {
+		return b, err
+	}
+	ast.Standardize()
+
+	return ast.Pack(), nil
 }
 
 // launch bot with given parameters
